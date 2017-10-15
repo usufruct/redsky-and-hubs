5b0187f0b2169dabee97e07361c78efed55ab046
Michael A Anderson <usufruct@gmail.com>
2017-10-15 17:47:33 -0500

    process wating for 'answer' will time out

--- second/worker.py
+++ second/worker.py
@@ -9,7 +9,6 @@ import random
 queue = redis.StrictRedis(host='localhost', port=os.environ.get('REDIS_PORT'), db=0)
 
 while True:
-    print('olleh')
     results = queue.brpop('responsive')
     results_dict = json.loads(results[1])
     response_key = results_dict["respond_to"]
@@ -17,12 +16,12 @@ while True:
     reversed_param = param[::-1]
 
     # random sleep from .001 -> 2 seconds
-    sleep_time = random.randint(1, 2000)
+    sleep_time = random.randint(1, 1000)
     print("sleeping for {} milliseconds".format(sleep_time))
+    print(response_key)
+    sys.stdout.flush()
 
     time.sleep(sleep_time/1000)
 
     queue.hset('response_store', response_key, reversed_param)
-    print(response_key)
-    sys.stdout.flush()
 
--- second/writer.py
+++ second/writer.py
@@ -4,9 +4,16 @@ import os
 import redis
 import uuid
 import json
+import signal  # unix only
 
 
 queue = redis.StrictRedis(host='localhost', port=os.environ.get('REDIS_PORT'), db=0)
+
+class TimeoutException(Exception):
+    pass
+
+def timeout_handler(num, stack):
+    raise TimeoutException("it took TOO LONG!")
 
 def task():
     task = {}
@@ -16,8 +23,11 @@ def task():
 
     return task
 
+signal.signal(signal.SIGALRM, timeout_handler)
+
 while True:
-    print('hello')
+    print('back to top')
+    sys.stdout.flush()
 
     new_task = task()
     respond_to = new_task["respond_to"]
@@ -27,11 +37,27 @@ while True:
 
     answer = queue.hget('response_store', respond_to)
 
-    while(answer == None):
-        time.sleep(0.01)
-        answer = queue.hget('response_store', respond_to)
+    try:
+        signal.alarm(1)
+        print("looking for {}".format(respond_to))
+        sys.stdout.flush()
+        while(answer == None):
+            time.sleep(0.01)
+            answer = queue.hget('response_store', respond_to)
+    except TimeoutException as e:
+        print('operation timed out')
+        print(respond_to)
+        sys.stdout.flush()
 
-    print('answer found')
-    print(answer)
+    if(answer != None):
+        print('answer found')
+        print(respond_to)
+        print(answer)
+        sys.stdout.flush()
 
+    print('resetting alarm')
     sys.stdout.flush()
+    signal.alarm(0)
+
+
+
