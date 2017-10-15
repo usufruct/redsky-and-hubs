51b1b3fc021ee6ca2ec0b584bbf080dff1c0405d
Michael A Anderson <usufruct@gmail.com>
2017-10-15 16:35:44 -0500

    second example worker waits randomly

--- second/worker.py
+++ second/worker.py
@@ -3,6 +3,7 @@ import sys
 import os
 import redis
 import json
+import random
 
 
 queue = redis.StrictRedis(host='localhost', port=os.environ.get('REDIS_PORT'), db=0)
@@ -15,7 +16,11 @@ while True:
     param = results_dict["params"]["uuid"]
     reversed_param = param[::-1]
 
-    time.sleep(2)
+    # random sleep from .001 -> 2 seconds
+    sleep_time = random.randint(1, 2000)
+    print("sleeping for {} milliseconds".format(sleep_time))
+
+    time.sleep(sleep_time/1000)
 
     queue.hset('response_store', response_key, reversed_param)
     print(response_key)
